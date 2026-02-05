66c938850f21dd3e3911e7cdcce6e2bda4b4a0a9
Miguel Ortiz <miangeltiz20@gmail.com>
2026-02-05 18:45:36 -0300

    [dev] Miguel - adicionando variaveis

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,9 @@
-print("Hola, mundo!")
+import os
+
+def main():
+    nombre = os.getenv("USERNAME")
+    print(f"¡Hola, {nombre} desde GitHub!")
+    print('Muy interesante!')
+
+if __name__ == "__main__":
+    main()
